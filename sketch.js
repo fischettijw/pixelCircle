cce22a866cd2543745cdc8cc1ea00a2af6fa3d1f
Joseph Fischetti <fischettijw@gmail.com>
2020-10-28 17:10:53 -0400

    looped both x & y

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ function initialize() {
     gridWidth = 750;
     cellNum = 15;
     cellSize = gridWidth / cellNum; //force canvas to stay the same size
-    cycles = 0;
+    cycles = 5;
     pieDiv = createDiv().style('font-size', '14pt');
 }
 
@@ -19,41 +19,36 @@ function setup() {
 
 function draw() {
     if (cycles < 1) {
-        // strokeGridWeight = 0;
-        strokeGridWeight = 1;
+        strokeGridWeight = 0;
         noLoop();
     }
     drawGrid(cellSize, cellNum);
     fill('yellow');
     squCtr(0, 0, cellSize, cellNum);
-    drawCircle(floor(cellNum / 2), cellSize);
+    drawCircle(cellNum, cellSize);
     cellNum = cellNum * 2 + 1;
     cellSize = gridWidth / cellNum;
     output();
     cycles--;
 }
 
-function drawCircle(radius, cWidth) {
+function drawCircle(cNum, cWidth) {
+    let radius = floor(cNum / 2);
     fill('red');
-    for (let y = -floor(cellNum / 2); y <= floor(cellNum / 2); y++) {
-        squCtr(Math.round(Math.sqrt((radius * radius) - (y * y))), y, cWidth, cellNum);
-        squCtr(-Math.round(Math.sqrt((radius * radius) - (y * y))), y, cWidth, cellNum);
+
+    for (let x = 0; x <= floor(cNum / 2); x++) {
+        squCtr(round(sqrt((radius * radius) - (x * x))), x, cWidth, cNum); // UR
+        squCtr(round(-sqrt((radius * radius) - (x * x))), x, cWidth, cNum); // UL
+        squCtr(round(sqrt((radius * radius) - (x * x))), -x, cWidth, cNum); // LR
+        squCtr(round(-sqrt((radius * radius) - (x * x))), -x, cWidth, cNum); // LL
     };
 
-    if (cellSize > 25) { // fudge
-        squCtr(1, 7, cWidth, cellNum);
-        squCtr(1, -7, cWidth, cellNum);
-        squCtr(2, 7, cWidth, cellNum);
-        squCtr(2, -7, cWidth, cellNum);
-        squCtr(3, 6, cWidth, cellNum);
-        squCtr(3, -6, cWidth, cellNum);
-        squCtr(-1, 7, cWidth, cellNum);
-        squCtr(-1, -7, cWidth, cellNum);
-        squCtr(-2, 7, cWidth, cellNum);
-        squCtr(-2, -7, cWidth, cellNum);
-        squCtr(-3, 6, cWidth, cellNum);
-        squCtr(-3, -6, cWidth, cellNum);
-    }
+    for (let y = 0; y <= floor(cNum / 2); y++) {
+        squCtr(y, round(sqrt((radius * radius) - (y * y))), cWidth, cNum); // UR
+        squCtr(y, round(-sqrt((radius * radius) - (y * y))), cWidth, cNum); // UL
+        squCtr(-y, round(sqrt((radius * radius) - (y * y))), cWidth, cNum); // LR
+        squCtr(-y, round(-sqrt((radius * radius) - (y * y))), cWidth, cNum); // LL
+    };
 }
 
 function drawGrid(cWidth, cNum) {
@@ -72,5 +67,5 @@ function squCtr(x, y, size, num) {
 }
 
 function output() {
-    pieDiv.html(`Cycles: ${cycles} - Size: ${nf(cellSize,0,4)}`);
+    pieDiv.html(`Diameter: ${cellNum} - Cell Size: ${nf(cellSize,0,4)}`);
 }
